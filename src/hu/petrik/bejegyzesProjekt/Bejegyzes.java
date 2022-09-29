31ddd098e6a617677c26310ec0cc723b41dadff8
Moravcsik Márk <moravcsikmark@gmail.com>
2022-09-29 11:29:14 +0200

    File Reading

--- src/hu/petrik/bejegyzesProjekt/Bejegyzes.java
+++ src/hu/petrik/bejegyzesProjekt/Bejegyzes.java
@@ -11,7 +11,7 @@ public class Bejegyzes {
     private LocalDateTime letrejott;
     private LocalDateTime szerkesztve;
 
-    public Bejegyzes(String szerzo, String tartalom, int likeok, LocalDateTime letrejott, LocalDateTime szerkesztve) {
+    public Bejegyzes(String szerzo, String tartalom) {// ,int likeok, LocalDateTime letrejott, LocalDateTime szerkesztve
         this.szerzo = szerzo;
         this.tartalom = tartalom;
         this.likeok = 0;
@@ -19,5 +19,64 @@ public class Bejegyzes {
         this.szerkesztve = LocalDateTime.now();
     }
 
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public void setSzerzo(String szerzo) {
+        this.szerzo = szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public void setLikeok(int likeok) {
+        this.likeok = likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public void setLetrejott(LocalDateTime letrejott) {
+        this.letrejott = letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
+    }
+
+    public void setSzerkesztve(LocalDateTime szerkesztve) {
+        this.szerkesztve = szerkesztve;
+        if (szerkesztve!=letrejott){
+            letrejott = szerkesztve;
+        }
+    }
+    public void like(){
+        likeok++;
+    }
+
+    @Override
+    public String toString() {
+        return "Bejegyzes{" +
+                "szerzo='" + szerzo + '\'' +
+                ", likeok=" + likeok +
+                ", letrejott=" + letrejott +
+
+
+                ", szerkesztve=" + szerkesztve +
+                ", tartalom='" + tartalom + '\'' +
+
+                '}';
+    }
 }
 
--- src/hu/petrik/bejegyzesProjekt/BejegyzesProject.java
+++ src/hu/petrik/bejegyzesProjekt/BejegyzesProject.java
@@ -1,8 +1,79 @@
 package hu.petrik.bejegyzesProjekt;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class BejegyzesProject {
 
     public static void main(String[] args) {
+        List<Bejegyzes> UserList = new ArrayList<Bejegyzes>();
+        Scanner sc = new Scanner(System.in);
+        int fix = 0;
+        System.out.println("Kérlek adj meg egy számot hogy hány uj bejegyzést adjak hozzá: ");
+        int num = sc.nextInt();
+        sc.nextLine();
+
+        if (num < 0) {
+            System.out.println("Nem megfelelő shzámot vagy nem számot adtál meg!");
+        } else {
+            for (int i = 0; i <= num; i++) {
+                UserList.add(new Bejegyzes("szerzo", "tartalom"));
+            }
+        }
+        sc.close();
+        Scanner bc = new Scanner("bejegyzesek.csv");
+        while (bc.hasNext()) {
+            String[] line = bc.nextLine().split(";");
+            UserList.add(new Bejegyzes(line[0], line[1]));
+        }
+
+        for (int i = 0; i < UserList.size() * 20; i++) {
+            UserList.get(i).setLikeok(+1);
+        }
+
+        System.out.println("Kérlek adj meg egy szöveget meg egy szöveget ");
+        String edit = bc.nextLine();
+        for (int i = 0; i < UserList.size(); i++) {
+            int a = 2;
+
+        }
+        bc.close();
+        for (int i = 0; i < UserList.size(); i++) {
+            System.out.println(UserList.get(i));
+        }
+
+        //2.
+        int f = 0;
+        for (int i = 0; i < UserList.size(); i++) {
+            if (UserList.get(i).getLikeok() < f) {
+                f = UserList.get(i).getLikeok();
+                System.out.println("Ezek a likeok száma:" + f +"/n");
+            }
+
+
+        }
+        int g =35;
+        for (int i = 0; i < UserList.size() ; i++) {
+            if (g <=UserList.get(i).getLikeok() );
+            {
+                System.out.println("van olyan bejegyzés ami 35 likeot kapoott ");
+            }
+        }
 
     }
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
